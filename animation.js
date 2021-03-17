039be5b0c33def12e4b82413befb131e2953b1cf
priyansh <singhalpriyansh58@gmail.com>
2021-03-17 17:16:03 +0530

    Animation class Added

---
+++ animation.js
@@ -0,0 +1,27 @@
+
+const icons = [
+    ".left-analytics",
+    ".left-forex",
+    ".left-investment",
+    ".left-saving",
+    ".left-card",
+    ".left-reward",
+    ".left-loan",
+    ".left-cash",
+    ".left-support"
+]
+
+var i = 0;
+
+for (var i = 0; i<icons.length; i++){
+    const domIcon = document.querySelector(icons[i]);
+}
+
+icons.map(function(icon){
+    // add class add-animation 
+})
+
+setTimeout(()=>{
+    console.log("mai aaagaya")
+    icon.style.color = "orange";
+}, 5000)
